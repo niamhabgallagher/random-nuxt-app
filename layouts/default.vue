5b3c03c05d8e5463f75338aae4ee48606bfcffe1
Niamh Gallagher <ngallagher@vtime.local>
2023-02-07 16:20:13 +0000

    Inital Commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title>Random Nuxt App</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer style="background-color: #fff;">
+      <v-btn color="primary" style="margin: 10px;" fab>
+        <v-icon>mdi-format-list-bulleted</v-icon>
+      </v-btn>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      items: [
+        { title: 'Loading 1', num: 1, color: 'primary' },
+        { title: 'Loading 2', num: 2, color: 'secondary' },
+        { title: 'Loading 3', num: 3, color: 'tertiary' },
+        { title: 'Loading 4', num: 4, color: 'success' },
+      ]
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card>
+    <v-card-title>New App</v-card-title>
+    <v-card-text>New App text</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
