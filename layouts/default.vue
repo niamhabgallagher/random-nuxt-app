50d52eb12dd5952cf6901800d43bccc3d3b66d79
Niamh Gallagher <ngallagher@vtime.local>
2023-02-08 13:54:57 +0000

    Fab button with loading items

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,44 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer style="background-color: #fff;">
-      <v-badge color="red" :content="items.length" overlap>
-        <v-icon color="primary" style="margin: 10px;" large>mdi-format-list-bulleted</v-icon>
-      </v-badge>
-    </v-footer>
+    <!-- <v-footer style="background-color: #fff;">
+      <v-btn class="mx-2" fab dark color="teal" style="margin: 20px">
+        <v-badge color="red" :content="items.length" >
+          <v-icon dark>
+            mdi-format-list-bulleted-square
+          </v-icon>
+        </v-badge>
+      </v-btn>
+    </v-footer> -->
+    <v-speed-dial 
+      v-model="fab"
+      bottom left
+      direction="top"
+      style="width: 15%"
+    >
+    <template #activator>
+      <v-btn v-model="fab" color="blue darken-2" dark fab>
+        <v-badge color="red" :content="items.length">
+          <v-icon v-if="fab" dark>
+            mdi-close
+          </v-icon>
+          <v-icon v-else dark>
+            mdi-format-list-bulleted-square
+          </v-icon>
+        </v-badge>
+      </v-btn>
+    </template>
+    <v-item-group style="width: 100%">
+      <v-item v-for="item in items" :key="item.num" style="margin: 15px">
+        <v-card>
+          <v-card-title>{{ item.title }}</v-card-title>
+          <v-card-text>
+            <v-progress-linear indeterminate :color="item.color"></v-progress-linear>
+          </v-card-text>
+        </v-card>
+      </v-item>
+    </v-item-group>
+  </v-speed-dial>
   </v-app>
 </template>
 
@@ -24,9 +57,10 @@ export default {
       items: [
         { title: 'Loading 1', num: 1, color: 'primary' },
         { title: 'Loading 2', num: 2, color: 'secondary' },
-        { title: 'Loading 3', num: 3, color: 'tertiary' },
+        { title: 'Loading 3', num: 3, color: 'warning' },
         { title: 'Loading 4', num: 4, color: 'success' },
-      ]
+      ],
+      fab: false
     }
   },
 }
