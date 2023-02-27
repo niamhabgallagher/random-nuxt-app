569f76143a0bb0d51acd80b012478be78f7e955b
Niamh Gallagher <n.gallagher@vtime.net>
2023-02-27 10:36:49 +0000

    Card with background image and some styling

--- pages/Work.vue
+++ pages/Work.vue
@@ -20,7 +20,7 @@
           </v-card-text>
         </v-col>
         <v-col cols="4">
-          <v-avatar width="350px" height="350px">
+          <v-avatar width="100%" height="90%">
             <img src="https://images.unsplash.com/photo-1675652426417-a7305afe7dbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
           </v-avatar>
         </v-col>
@@ -33,8 +33,8 @@
       <v-card-text>
         <v-card-text>
           <v-row>
-            <v-col v-for="item in items" :key="item.name" cols="6">
-              <v-card color="#fbfcfa">
+            <v-col v-for="item in items" :key="item.name" cols="12" md="6">
+              <v-card color="#fbfcfa" height="220px">
                 <v-card-title><h2>{{ item.name }}</h2></v-card-title>
                 <v-card-subtitle>{{ item.type }}</v-card-subtitle>
                 <v-card-text>
@@ -45,6 +45,41 @@
             </v-col>
           </v-row>
         </v-card-text>
+      </v-card-text>
+    </v-card>
+    <v-card class="card-image-background">
+      <v-card-title>
+        <h1>For Clients</h1>
+      </v-card-title>
+      <v-card-subtitle style="max-width: 500px"><h2 class="subtitle">Find talent your way</h2></v-card-subtitle>
+      <v-card-text style="max-width: 500px; font-size: 20px;font-weight: bold;margin-top: 75px;color: white;">Work with the largest network of independent professionals and get things done—from quick turnarounds to big transformations.</v-card-text>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <v-card color="#f9fef6">
+              <v-card-title style="font-size: 40px;">Post a job and hire a pro</v-card-title>
+              <v-card-text>
+                <v-btn color="primary" rounded><b>View More</b></v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card color="#f9fef6">
+              <v-card-title style="font-size: 40px;">Post a job and hire a pro</v-card-title>
+              <v-card-text>
+                <v-btn color="primary" rounded><b>View More</b></v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card color="#f9fef6">
+              <v-card-title style="font-size: 40px;">Post a job and hire a pro</v-card-title>
+              <v-card-text>
+                <v-btn color="primary" rounded><b>View More</b></v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-container>
@@ -70,7 +105,7 @@ export default {
 <style scoped>
 .page-title {
   font-size: 75px;
-  line-height: 1;
+  line-height: 1.2;
   max-width: 525px !important;
   color: rgb(0, 138, 88);
   /* white-space: break-spaces; */
@@ -80,5 +115,16 @@ export default {
   font-size: 30px;
   margin-top: 20px;
   margin-bottom: 40px !important;
+}
+.card-image-background {
+  background-image: url('https://images.unsplash.com/photo-1598257006458-087169a1f08d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80');
+  background-size: cover;
+  font-family: 'Times New Roman', Times, serif;
+}
+.subtitle {
+  margin-top: 50px;
+  font-size: 90px;
+  color: black;
+  line-height: 1.2;
 }
 </style>
